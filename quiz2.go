8f971c4dfe74356764ff3fecae6b44ad26f1bb17
Patrick090747 <1289453297@qq.com>
2022-05-28 22:44:12 +0800

    Changes to be committed: 	modified:   quiz2.go 	修改为sleep方法，并且实现一发一收

--- quiz2.go
+++ quiz2.go
@@ -9,7 +9,7 @@ func producer(out chan<- int) {
 	for i := 0; i < 10; i++ {
 		data := i
 		fmt.Println("生产者生产数据:", data)
-		time.Tick(time.Second)
+		time.Sleep(time.Second)
 		out <- data // 缓冲区写入数据
 	}
 	close(out) //写完关闭管道
@@ -17,8 +17,9 @@ func producer(out chan<- int) {
 
 func consumer(in <-chan int) {
 	for data := range in {
-		time.Tick(time.Second)
+
 		fmt.Println("消费者得到数据：", data)
+		time.Sleep(time.Second)
 	}
 
 }
